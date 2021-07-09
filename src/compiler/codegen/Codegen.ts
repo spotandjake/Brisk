5609f05f654b56916c733db45d933d494c0e44fa
Jake Follest <40705786+spotandjake@users.noreply.github.com>
2021-07-09 15:13:17 -0400

    get startedd on implementing codegeneration, loop back to type checking and module linkinng

---
+++ src/compiler/codegen/Codegen.ts
@@ -0,0 +1,8 @@
+// dependency's
+import binaryen from 'binaryen';
+// type's
+// import { ParseTreeNode, ProgramNode, FlagStatementNode } from '../Grammar/Types';
+// codeGen
+let CodeGen = (code) => {
+
+}
