3a08f97ea1a19a31ffdedaad780461a7d44094d9
Spotandjake <spotandjake@hotmail.com>
2022-05-08 17:52:30 -0400

    Start The Wasm Module Assembler

--- src/wasmBuilder/Output/wasm.ts
+++ src/wasmBuilder/Output/wasm.ts
@@ -1,5 +1,11 @@
 import { WasmModuleType } from '../Types/Nodes';
-// TODO: Implement Wasm Builder
+// Helpers
+const magicModuleHeader = [0x00, 0x61, 0x73, 0x6d];
+const moduleVersion = [0x01, 0x00, 0x00, 0x00];
+// TODO: Consider Using https://github.com/ColinEberhardt/chasm/blob/master/src/emitter.ts#L296 And Expanding It
+// Implement Wasm Builder
 export const compileWasm = (wasmModule: WasmModuleType): Uint8Array => {
-  return new Uint8Array([]); // TODO: Implement Building
+  // TODO: Build Module Contents
+  // Return Module
+  return Uint8Array.from([...magicModuleHeader, ...moduleVersion]);
 };
