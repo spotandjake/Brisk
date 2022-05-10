b89f719ef4ab839841796d2cd3578312a161268a
Spotandjake <spotandjake@hotmail.com>
2022-05-10 15:51:34 -0400

    Implement More Wasm Expressions For WasmBuilder IR

--- src/wasmBuilder/Build/Expression.ts
+++ src/wasmBuilder/Build/Expression.ts
@@ -3,12 +3,41 @@ import {
   CallExpression,
   CallIndirectExpression,
   DropExpression,
+  F32_ConstExpression,
+  F32_LoadExpression,
+  F32_StoreExpression,
+  F64_ConstExpression,
+  F64_LoadExpression,
+  F64_StoreExpression,
   Global_GetExpression,
   Global_SetExpression,
+  I32_ConstExpression,
+  I32_Load16_SExpression,
+  I32_Load16_UExpression,
+  I32_Load8_SExpression,
+  I32_Load8_UExpression,
+  I32_LoadExpression,
+  I32_Store16Expression,
+  I32_Store8Expression,
+  I32_StoreExpression,
+  I64_ConstExpression,
+  I64_Load16_SExpression,
+  I64_Load16_UExpression,
+  I64_Load32_SExpression,
+  I64_Load32_UExpression,
+  I64_Load8_SExpression,
+  I64_Load8_UExpression,
+  I64_LoadExpression,
+  I64_Store16Expression,
+  I64_Store32Expression,
+  I64_Store8Expression,
+  I64_StoreExpression,
   IfExpression,
   Local_GetExpression,
   Local_SetExpression,
   Local_TeeExpression,
+  Memory_GrowExpression,
+  Memory_SizeExpression,
   NopExpression,
   ReturnExpression,
   UnreachableExpression,
@@ -126,5 +155,336 @@ export const global_SetExpression = (
     nodeType: WasmExpressions.global_setExpr,
     globalName: globalName,
     body: body,
+  };
+};
+export const i32_LoadExpression = (
+  offset: number,
+  align: number,
+  ptr: WasmExpression
+): I32_LoadExpression => {
+  return {
+    nodeType: WasmExpressions.i32_loadExpr,
+    offset: offset,
+    align: align,
+    ptr: ptr,
+  };
+};
+export const i64_LoadExpression = (
+  offset: number,
+  align: number,
+  ptr: WasmExpression
+): I64_LoadExpression => {
+  return {
+    nodeType: WasmExpressions.i64_loadExpr,
+    offset: offset,
+    align: align,
+    ptr: ptr,
+  };
+};
+export const f32_LoadExpression = (
+  offset: number,
+  align: number,
+  ptr: WasmExpression
+): F32_LoadExpression => {
+  return {
+    nodeType: WasmExpressions.f32_loadExpr,
+    offset: offset,
+    align: align,
+    ptr: ptr,
+  };
+};
+export const f64_LoadExpression = (
+  offset: number,
+  align: number,
+  ptr: WasmExpression
+): F64_LoadExpression => {
+  return {
+    nodeType: WasmExpressions.f64_loadExpr,
+    offset: offset,
+    align: align,
+    ptr: ptr,
+  };
+};
+export const i32_Load8_SExpression = (
+  offset: number,
+  align: number,
+  ptr: WasmExpression
+): I32_Load8_SExpression => {
+  return {
+    nodeType: WasmExpressions.i32_load8_sExpr,
+    offset: offset,
+    align: align,
+    ptr: ptr,
+  };
+};
+export const i32_Load8_UExpression = (
+  offset: number,
+  align: number,
+  ptr: WasmExpression
+): I32_Load8_UExpression => {
+  return {
+    nodeType: WasmExpressions.i32_load8_uExpr,
+    offset: offset,
+    align: align,
+    ptr: ptr,
+  };
+};
+export const i32_Load16_SExpression = (
+  offset: number,
+  align: number,
+  ptr: WasmExpression
+): I32_Load16_SExpression => {
+  return {
+    nodeType: WasmExpressions.i32_load16_sExpr,
+    offset: offset,
+    align: align,
+    ptr: ptr,
+  };
+};
+export const i32_Load16_UExpression = (
+  offset: number,
+  align: number,
+  ptr: WasmExpression
+): I32_Load16_UExpression => {
+  return {
+    nodeType: WasmExpressions.i32_load16_uExpr,
+    offset: offset,
+    align: align,
+    ptr: ptr,
+  };
+};
+export const i64_Load8_SExpression = (
+  offset: number,
+  align: number,
+  ptr: WasmExpression
+): I64_Load8_SExpression => {
+  return {
+    nodeType: WasmExpressions.i64_load8_sExpr,
+    offset: offset,
+    align: align,
+    ptr: ptr,
+  };
+};
+export const i64_Load8_UExpression = (
+  offset: number,
+  align: number,
+  ptr: WasmExpression
+): I64_Load8_UExpression => {
+  return {
+    nodeType: WasmExpressions.i64_load8_uExpr,
+    offset: offset,
+    align: align,
+    ptr: ptr,
+  };
+};
+export const i64_Load16_SExpression = (
+  offset: number,
+  align: number,
+  ptr: WasmExpression
+): I64_Load16_SExpression => {
+  return {
+    nodeType: WasmExpressions.i64_load16_sExpr,
+    offset: offset,
+    align: align,
+    ptr: ptr,
+  };
+};
+export const i64_Load16_UExpression = (
+  offset: number,
+  align: number,
+  ptr: WasmExpression
+): I64_Load16_UExpression => {
+  return {
+    nodeType: WasmExpressions.i64_load16_uExpr,
+    offset: offset,
+    align: align,
+    ptr: ptr,
+  };
+};
+export const i64_Load32_SExpression = (
+  offset: number,
+  align: number,
+  ptr: WasmExpression
+): I64_Load32_SExpression => {
+  return {
+    nodeType: WasmExpressions.i64_load32_sExpr,
+    offset: offset,
+    align: align,
+    ptr: ptr,
+  };
+};
+export const i64_Load32_UExpression = (
+  offset: number,
+  align: number,
+  ptr: WasmExpression
+): I64_Load32_UExpression => {
+  return {
+    nodeType: WasmExpressions.i64_load32_uExpr,
+    offset: offset,
+    align: align,
+    ptr: ptr,
+  };
+};
+export const i32_StoreExpression = (
+  offset: number,
+  align: number,
+  ptr: WasmExpression,
+  value: WasmExpression
+): I32_StoreExpression => {
+  return {
+    nodeType: WasmExpressions.i32_storeExpr,
+    offset: offset,
+    align: align,
+    ptr: ptr,
+    value: value,
+  };
+};
+export const i64_StoreExpression = (
+  offset: number,
+  align: number,
+  ptr: WasmExpression,
+  value: WasmExpression
+): I64_StoreExpression => {
+  return {
+    nodeType: WasmExpressions.i64_storeExpr,
+    offset: offset,
+    align: align,
+    ptr: ptr,
+    value: value,
+  };
+};
+export const f32_StoreExpression = (
+  offset: number,
+  align: number,
+  ptr: WasmExpression,
+  value: WasmExpression
+): F32_StoreExpression => {
+  return {
+    nodeType: WasmExpressions.f32_storeExpr,
+    offset: offset,
+    align: align,
+    ptr: ptr,
+    value: value,
+  };
+};
+export const f64_StoreExpression = (
+  offset: number,
+  align: number,
+  ptr: WasmExpression,
+  value: WasmExpression
+): F64_StoreExpression => {
+  return {
+    nodeType: WasmExpressions.f64_storeExpr,
+    offset: offset,
+    align: align,
+    ptr: ptr,
+    value: value,
+  };
+};
+export const i32_Store8Expression = (
+  offset: number,
+  align: number,
+  ptr: WasmExpression,
+  value: WasmExpression
+): I32_Store8Expression => {
+  return {
+    nodeType: WasmExpressions.i32_store8Expr,
+    offset: offset,
+    align: align,
+    ptr: ptr,
+    value: value,
+  };
+};
+export const i32_Store16Expression = (
+  offset: number,
+  align: number,
+  ptr: WasmExpression,
+  value: WasmExpression
+): I32_Store16Expression => {
+  return {
+    nodeType: WasmExpressions.i32_store16Expr,
+    offset: offset,
+    align: align,
+    ptr: ptr,
+    value: value,
+  };
+};
+export const i64_Store8Expression = (
+  offset: number,
+  align: number,
+  ptr: WasmExpression,
+  value: WasmExpression
+): I64_Store8Expression => {
+  return {
+    nodeType: WasmExpressions.i64_store8Expr,
+    offset: offset,
+    align: align,
+    ptr: ptr,
+    value: value,
+  };
+};
+export const i64_Store16Expression = (
+  offset: number,
+  align: number,
+  ptr: WasmExpression,
+  value: WasmExpression
+): I64_Store16Expression => {
+  return {
+    nodeType: WasmExpressions.i64_store16Expr,
+    offset: offset,
+    align: align,
+    ptr: ptr,
+    value: value,
+  };
+};
+export const i64_Store32Expression = (
+  offset: number,
+  align: number,
+  ptr: WasmExpression,
+  value: WasmExpression
+): I64_Store32Expression => {
+  return {
+    nodeType: WasmExpressions.i64_store32Expr,
+    offset: offset,
+    align: align,
+    ptr: ptr,
+    value: value,
+  };
+};
+export const memory_SizeExpression = (): Memory_SizeExpression => {
+  return {
+    nodeType: WasmExpressions.memory_sizeExpr,
+  };
+};
+// TODO: Ensure value isn't defined at runtime and doesn't take a wasmExpression
+export const memory_GrowExpression = (value: number): Memory_GrowExpression => {
+  return {
+    nodeType: WasmExpressions.memory_growExpr,
+    value: value,
+  };
+};
+export const i32_ConstExpression = (value: number): I32_ConstExpression => {
+  return {
+    nodeType: WasmExpressions.i32_constExpr,
+    value: value,
+  };
+};
+// TODO: Switch This to BigInt
+export const i64_ConstExpression = (value: number): I64_ConstExpression => {
+  return {
+    nodeType: WasmExpressions.i64_constExpr,
+    value: value,
+  };
+};
+export const f32_ConstExpression = (value: number): F32_ConstExpression => {
+  return {
+    nodeType: WasmExpressions.f32_constExpr,
+    value: value,
+  };
+};
+export const f64_ConstExpression = (value: number): F64_ConstExpression => {
+  return {
+    nodeType: WasmExpressions.f64_constExpr,
+    value: value,
   };
 };
--- src/wasmBuilder/Types/Nodes.ts
+++ src/wasmBuilder/Types/Nodes.ts
@@ -49,6 +49,35 @@ export const enum WasmExpressions {
   global_setExpr,
   // TODO: table_get
   // TODO: table_set
+  i32_loadExpr,
+  i64_loadExpr,
+  f32_loadExpr,
+  f64_loadExpr,
+  i32_load8_sExpr,
+  i32_load8_uExpr,
+  i32_load16_sExpr,
+  i32_load16_uExpr,
+  i64_load8_sExpr,
+  i64_load8_uExpr,
+  i64_load16_sExpr,
+  i64_load16_uExpr,
+  i64_load32_sExpr,
+  i64_load32_uExpr,
+  i32_storeExpr,
+  i64_storeExpr,
+  f32_storeExpr,
+  f64_storeExpr,
+  i32_store8Expr,
+  i32_store16Expr,
+  i64_store8Expr,
+  i64_store16Expr,
+  i64_store32Expr,
+  memory_sizeExpr,
+  memory_growExpr,
+  i32_constExpr,
+  i64_constExpr,
+  f32_constExpr,
+  f64_constExpr,
 }
 export interface UnreachableExpression {
   nodeType: WasmExpressions.unreachableExpr;
@@ -116,6 +145,179 @@ export interface Global_SetExpression {
 }
 // TODO: table_get
 // TODO: table_set
+export interface I32_LoadExpression {
+  nodeType: WasmExpressions.i32_loadExpr;
+  offset: number;
+  align: number;
+  ptr: WasmExpression;
+}
+export interface I64_LoadExpression {
+  nodeType: WasmExpressions.i64_loadExpr;
+  offset: number;
+  align: number;
+  ptr: WasmExpression;
+}
+export interface F32_LoadExpression {
+  nodeType: WasmExpressions.f32_loadExpr;
+  offset: number;
+  align: number;
+  ptr: WasmExpression;
+}
+export interface F64_LoadExpression {
+  nodeType: WasmExpressions.f64_loadExpr;
+  offset: number;
+  align: number;
+  ptr: WasmExpression;
+}
+export interface I32_Load8_SExpression {
+  nodeType: WasmExpressions.i32_load8_sExpr;
+  offset: number;
+  align: number;
+  ptr: WasmExpression;
+}
+export interface I32_Load8_UExpression {
+  nodeType: WasmExpressions.i32_load8_uExpr;
+  offset: number;
+  align: number;
+  ptr: WasmExpression;
+}
+export interface I32_Load16_SExpression {
+  nodeType: WasmExpressions.i32_load16_sExpr;
+  offset: number;
+  align: number;
+  ptr: WasmExpression;
+}
+export interface I32_Load16_UExpression {
+  nodeType: WasmExpressions.i32_load16_uExpr;
+  offset: number;
+  align: number;
+  ptr: WasmExpression;
+}
+export interface I64_Load8_SExpression {
+  nodeType: WasmExpressions.i64_load8_sExpr;
+  offset: number;
+  align: number;
+  ptr: WasmExpression;
+}
+export interface I64_Load8_UExpression {
+  nodeType: WasmExpressions.i64_load8_uExpr;
+  offset: number;
+  align: number;
+  ptr: WasmExpression;
+}
+export interface I64_Load16_SExpression {
+  nodeType: WasmExpressions.i64_load16_sExpr;
+  offset: number;
+  align: number;
+  ptr: WasmExpression;
+}
+export interface I64_Load16_UExpression {
+  nodeType: WasmExpressions.i64_load16_uExpr;
+  offset: number;
+  align: number;
+  ptr: WasmExpression;
+}
+export interface I64_Load32_SExpression {
+  nodeType: WasmExpressions.i64_load32_sExpr;
+  offset: number;
+  align: number;
+  ptr: WasmExpression;
+}
+export interface I64_Load32_UExpression {
+  nodeType: WasmExpressions.i64_load32_uExpr;
+  offset: number;
+  align: number;
+  ptr: WasmExpression;
+}
+
+export interface I32_StoreExpression {
+  nodeType: WasmExpressions.i32_storeExpr;
+  offset: number;
+  align: number;
+  ptr: WasmExpression;
+  value: WasmExpression;
+}
+export interface I64_StoreExpression {
+  nodeType: WasmExpressions.i64_storeExpr;
+  offset: number;
+  align: number;
+  ptr: WasmExpression;
+  value: WasmExpression;
+}
+export interface F32_StoreExpression {
+  nodeType: WasmExpressions.f32_storeExpr;
+  offset: number;
+  align: number;
+  ptr: WasmExpression;
+  value: WasmExpression;
+}
+export interface F64_StoreExpression {
+  nodeType: WasmExpressions.f64_storeExpr;
+  offset: number;
+  align: number;
+  ptr: WasmExpression;
+  value: WasmExpression;
+}
+export interface I32_Store8Expression {
+  nodeType: WasmExpressions.i32_store8Expr;
+  offset: number;
+  align: number;
+  ptr: WasmExpression;
+  value: WasmExpression;
+}
+export interface I32_Store16Expression {
+  nodeType: WasmExpressions.i32_store16Expr;
+  offset: number;
+  align: number;
+  ptr: WasmExpression;
+  value: WasmExpression;
+}
+export interface I64_Store8Expression {
+  nodeType: WasmExpressions.i64_store8Expr;
+  offset: number;
+  align: number;
+  ptr: WasmExpression;
+  value: WasmExpression;
+}
+export interface I64_Store16Expression {
+  nodeType: WasmExpressions.i64_store16Expr;
+  offset: number;
+  align: number;
+  ptr: WasmExpression;
+  value: WasmExpression;
+}
+export interface I64_Store32Expression {
+  nodeType: WasmExpressions.i64_store32Expr;
+  offset: number;
+  align: number;
+  ptr: WasmExpression;
+  value: WasmExpression;
+}
+export interface Memory_SizeExpression {
+  nodeType: WasmExpressions.memory_sizeExpr;
+}
+export interface Memory_GrowExpression {
+  nodeType: WasmExpressions.memory_growExpr;
+  value: number;
+}
+export interface I32_ConstExpression {
+  nodeType: WasmExpressions.i32_constExpr;
+  value: number;
+}
+// TODO: Switch This to BigInt
+export interface I64_ConstExpression {
+  nodeType: WasmExpressions.i64_constExpr;
+  value: number;
+}
+export interface F32_ConstExpression {
+  nodeType: WasmExpressions.f32_constExpr;
+  value: number;
+}
+export interface F64_ConstExpression {
+  nodeType: WasmExpressions.f64_constExpr;
+  value: number;
+}
+// Wasm Expression Types
 export type WasmExpression =
   | UnreachableExpression
   | NopExpression
@@ -129,4 +331,33 @@ export type WasmExpression =
   | Local_SetExpression
   | Local_TeeExpression
   | Global_GetExpression
-  | Global_SetExpression;
+  | Global_SetExpression
+  | I32_LoadExpression
+  | I64_LoadExpression
+  | F32_LoadExpression
+  | F64_LoadExpression
+  | I32_Load8_SExpression
+  | I32_Load8_UExpression
+  | I32_Load16_SExpression
+  | I32_Load16_UExpression
+  | I64_Load8_SExpression
+  | I64_Load8_UExpression
+  | I64_Load16_SExpression
+  | I64_Load16_UExpression
+  | I64_Load32_SExpression
+  | I64_Load32_UExpression
+  | I32_StoreExpression
+  | I64_StoreExpression
+  | F32_StoreExpression
+  | F64_StoreExpression
+  | I32_Store8Expression
+  | I32_Store16Expression
+  | I64_Store8Expression
+  | I64_Store16Expression
+  | I64_Store32Expression
+  | Memory_SizeExpression
+  | Memory_GrowExpression
+  | I32_ConstExpression
+  | I64_ConstExpression
+  | F32_ConstExpression
+  | F64_ConstExpression;
